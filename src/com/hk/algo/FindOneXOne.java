8e34ed5dc0c0ca75a566f7a306db5d93bf8bad4e
kkth <hekun_golf@hotmail.com>
2014-12-29 22:41:26 +0800

    1

---
+++ src/com/hk/algo/FindOneXOne.java
@@ -0,0 +1,26 @@
+package com.hk.algo;
+/*
+ * Given a binary string, count number of substrings that start and end with 1. 
+ * For example, if the input string is ¡°00100101¡±, then there are 
+ * three substrings ¡°1001¡±, ¡°100101¡± and ¡°101¡±.
+ */
+public class FindOneXOne {
+
+	private String input = "00100101";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FindOneXOne find = new FindOneXOne();
+		find.cal();
+	}
+	
+	public void init()
+	{
+		
+	}
+	
+	public void cal()
+	{
+		
+	}
+
+}
