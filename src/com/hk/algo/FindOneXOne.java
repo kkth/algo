a282a595241a83dd8ba8f423721f093d956ce2e8
kkth <hekun_golf@hotmail.com>
2014-12-29 23:01:55 +0800

    2

--- src/com/hk/algo/FindOneXOne.java
+++ src/com/hk/algo/FindOneXOne.java
@@ -1,4 +1,8 @@
 package com.hk.algo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Given a binary string, count number of substrings that start and end with 1. 
  * For example, if the input string is ¡°00100101¡±, then there are 
@@ -6,7 +10,11 @@ package com.hk.algo;
  */
 public class FindOneXOne {
 
-	private String input = "00100101";
+	private String input = null;
+	private char[] inputArray = null;
+	private List<String> subStrings = new ArrayList<String>();
+	private int count = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FindOneXOne find = new FindOneXOne();
@@ -15,12 +23,42 @@ public class FindOneXOne {
 	
 	public void init()
 	{
-		
+		input = "00100101";
+		inputArray = input.toCharArray();
 	}
+	
 	
 	public void cal()
 	{
+		init();
+		for(int i =0;i<inputArray.length;i++)
+		{
+			if(inputArray[i]=='0')
+			{
+				continue;
+			}
+			else
+			{
+				for(int j =i+1;j<inputArray.length;j++)
+				{
+					if(inputArray[j]=='1')
+					{
+						String sub = new String(inputArray,i,j-i+1);
+						subStrings.add(sub);
+					}
+					count++;
+				}
+			}
+				
+		}
 		
+		System.out.println(input);
+		System.out.println("There are "+ subStrings.size() + " substrings.");
+		System.out.println("There are "+ count + " loops executed.");
+		for(String subStr:subStrings)
+		{
+			System.out.println(subStr);
+		}
 	}
 
 }
