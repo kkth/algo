003f387039c25efdda4a57e3ca4cd9854678058b
kkth <hekun_golf@hotmail.com>
2014-12-30 10:20:41 +0800

    Add resources

--- src/com/hk/algo/FindOneXOne.java
+++ src/com/hk/algo/FindOneXOne.java
@@ -1,7 +1,10 @@
 package com.hk.algo;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
  * Given a binary string, count number of substrings that start and end with 1. 
@@ -15,22 +18,28 @@ public class FindOneXOne {
 	private List<String> subStrings = new ArrayList<String>();
 	private int count = 0;
 	
+	private Map<Integer,List<SubStringIndex> > subStringMap = new HashMap<Integer,List<SubStringIndex> >();
+	private int lastOneIndex = -1;
+	private int count2 = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FindOneXOne find = new FindOneXOne();
-		find.cal();
+		find.init();
+		find.cal1();
+		find.cal2();
 	}
 	
 	public void init()
 	{
-		input = "00100101";
+		input = "001001010";
+		//input ="111111011";
 		inputArray = input.toCharArray();
 	}
 	
 	
-	public void cal()
+	public void cal1()
 	{
-		init();
 		for(int i =0;i<inputArray.length;i++)
 		{
 			if(inputArray[i]=='0')
@@ -59,6 +68,86 @@ public class FindOneXOne {
 		{
 			System.out.println(subStr);
 		}
+	}
+	
+	
+	//////////////////////////////////////////
+	class SubStringIndex
+	{
+		int beginInclude =-1;
+		int endExclude = -1;
+	}
+	
+	public void cal2()
+	{
+		System.out.println("Cal2======");
+		for(int i =0;i<inputArray.length;i++)
+		{
+			calBasedBefore(i);
+		}
+		
+		if(!subStringMap.isEmpty())
+		{
+			int count = 0;
+			Collection<List<SubStringIndex>> subStrings = subStringMap.values();
+			for(List<SubStringIndex> list: subStrings)
+			{
+				for(SubStringIndex index:list)
+				{
+					String subString = new String(inputArray,index.beginInclude,index.endExclude-index.beginInclude);
+					System.out.println(subString);
+					count ++;
+				}
+			}
+			
+			System.out.println("There are "+ count + " substrings.");
+		}
+	}
+	
+	public void calBasedBefore(int index)
+	{
+
+			
+		if(inputArray[index] == '0')
+		{	
+			return;
+		}
+		else
+		{
+			List<SubStringIndex> newIndexList = new ArrayList<SubStringIndex>();
+			
+			if(lastOneIndex==-1)
+			{
+				SubStringIndex newIndex = new SubStringIndex();
+				newIndex.beginInclude = index;
+				newIndex.endExclude = index+1;
+				newIndexList.add(newIndex);
+			}
+			else
+			{
+				List<SubStringIndex> subStrings = subStringMap.get(new Integer(lastOneIndex));
+	
+				for(SubStringIndex theIndex : subStrings)
+				{
+					SubStringIndex newIndex1 = new SubStringIndex();
+					newIndex1.beginInclude = theIndex.beginInclude;
+					newIndex1.endExclude = index+1;
+					newIndexList.add(newIndex1);
+					
+					SubStringIndex newIndex2 = new SubStringIndex();
+					newIndex2.beginInclude = theIndex.endExclude-1;
+					newIndex2.endExclude = index+1;
+					newIndexList.add(newIndex2);
+					
+				}
+			}
+			
+			subStringMap.put(new Integer(index), newIndexList);
+			lastOneIndex = index;
+			
+		}
+		
+			
 	}
 
 }
